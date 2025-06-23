fe2e663e822b2f17486455e34a09b62a105a22e9
Relton Lima <arfl@discente.ifpe.edu.br>
2025-06-23 16:47:06 -0300

    alter landing page and uniform api url os developer and production

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-// Caminho: aiot-saas-backend/server.js
-
+// server.js
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // Importa o módulo 'path' do Node.js
 const apiRoutes = require('./src/api/routes');
-const db = require('./src/config/db'); // Importamos o objeto db completo
-require('./src/services/mqttClient'); // Importa e inicializa o cliente MQTT
+require('./src/services/mqttClient');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuração do CORS
+// Configuração do CORS (continua útil para flexibilidade)
 const corsOptions = {
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
@@ -21,24 +20,23 @@ app.use(cors(corsOptions));
 // Middlewares padrões
 app.use(express.json());
 
-// Rota de teste
-app.get('/', (req, res) => {
-    res.send('API do AIoT SaaS v1.1 está no ar!');
-});
+// =====================================================================
+// === NOVA DIRETIVA PARA SERVIR O FRONTEND ===
+// =====================================================================
+// Diz ao Express para servir qualquer arquivo estático que esteja na pasta 'src/public'
+app.use(express.static(path.join(__dirname, 'src', 'public')));
+// =====================================================================
 
-// Rotas principais da API
+
+// Rotas principais da API (com prefixo /v1)
 app.use('/v1', apiRoutes);
 
 // --- NOVA ROTINA DE INICIALIZAÇÃO ---
 const startServer = async () => {
-    // 1. Testa a conexão com o banco de dados PRIMEIRO.
-    await db.testDbConnection();
-
-    // 2. Se a conexão com o banco for bem-sucedida, inicia o servidor web.
+    // db.testDbConnection() não é mais necessário aqui pois a conexão é feita sob demanda
     app.listen(PORT, () => {
-        console.log(`Servidor web rodando na porta ${PORT}`);
+        console.log(`Servidor full-stack rodando! Acesse o frontend em http://localhost:${PORT}/login.html`);
     });
 };
 
-// Inicia todo o processo.
 startServer();
